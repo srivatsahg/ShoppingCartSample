72d35a227b1f0bbfdf7e5bac98ab4327d33cc15f
Srivatsa Haridas <srivatsa.haridas@honeywell.com>
2020-09-18 22:45:46 +0530

    Initial test cases

---
+++ ShoppingLibraryTests/DiscountTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using Xunit;
+using ShoppingLibrary.Service;
+using ShoppingLibrary.Model;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+
+namespace ShoppingLibraryTests
+{
+    public class DiscountTests
+    {
+        private readonly Checkout _sut;
+
+        public DiscountTests()
+        {
+            _sut = new Checkout(new PromotionalOfferStrategy());
+        }
+
+        [Fact]
+        public void TestTotalCostWithPromotionalDiscount()
+        {
+            List<ShoppingCart> cart = new List<ShoppingCart>();
+
+            cart.Add(new ShoppingCart() { Product = new ShoppingProduct() { Name = "A", UnitPrice = 50.0 }, Quantity = 3 });
+            cart.Add(new ShoppingCart() { Product = new ShoppingProduct() { Name = "B", UnitPrice = 30.0 }, Quantity = 5 });
+            cart.Add(new ShoppingCart() { Product = new ShoppingProduct() { Name = "C", UnitPrice = 20.0 }, Quantity = 1 });
+            cart.Add(new ShoppingCart() { Product = new ShoppingProduct() { Name = "D", UnitPrice = 15.0 }, Quantity = 1 });
+
+            Assert.Equal(280, _sut.CalculateTotalCost(cart));
+        }
+    }
+}
---
+++ ShoppingLibraryTests/NonDiscountTests.cs
@@ -0,0 +1,32 @@
+﻿using ShoppingLibrary.Model;
+using ShoppingLibrary.Service;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ShoppingLibraryTests
+{
+    public class NonDiscountTests
+    {
+        private readonly Checkout _sut;
+
+        public NonDiscountTests()
+        {
+            _sut = new Checkout(new RegularOfferStrategy());
+        }
+
+        [Fact]
+        public void TestTotalCostWithNoDiscount()
+        {
+            List<ShoppingCart> cart = new List<ShoppingCart>();
+
+            cart.Add(new ShoppingCart() { Product = new ShoppingProduct() { Name = "A", UnitPrice = 50.0 }, Quantity = 1 });
+            cart.Add(new ShoppingCart() { Product = new ShoppingProduct() { Name = "B", UnitPrice = 30.0 }, Quantity = 1 });
+            cart.Add(new ShoppingCart() { Product = new ShoppingProduct() { Name = "C", UnitPrice = 20.0 }, Quantity = 1 });
+            cart.Add(new ShoppingCart() { Product = new ShoppingProduct() { Name = "D", UnitPrice = 15.0 }, Quantity = 1 });
+
+            Assert.Equal(115, _sut.CalculateTotalCost(cart));
+        }
+    }
+}
